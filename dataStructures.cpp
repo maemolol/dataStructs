f996c035c606c6e4a209a521f683f5b2e7d15ed0
mark <markkorobkin85@gmail.com>
2024-04-04 10:00:29 +0000

    main: - data structure sorting is added

--- dataStructures.cpp
+++ dataStructures.cpp
@@ -66,6 +66,51 @@ class LinkedList {
             }
             head = last;
         }
+
+        void sortList() {
+            Node *last = nullptr;
+            Node *current = head;
+            Node *next = current->next;
+            Node *new_head = head;
+            Node *min = head;
+            Node *before_min = last;
+            Node *after_min = next;
+            Node *unsorted = head;
+            Node *sorted = nullptr;
+            
+            while(true) {
+                current = unsorted;
+                while(current != nullptr) {
+                    if(current->data <= min->data) {
+                        before_min = last;
+                        min = current;
+                        after_min = next;
+                    }
+                    last = current;
+                    current = current->next;
+                    if(current != nullptr) {
+                        next = current->next;
+                    }
+                }
+                if(min == unsorted) {
+                    unsorted = unsorted->next;
+                }
+                min->next = new_head;
+                if(sorted == nullptr) {
+                    sorted = min;
+                }
+                new_head = min;
+                before_min->next = after_min;
+                min = unsorted;
+                if(min == nullptr) {
+                    break;
+                }
+
+                next = unsorted->next;
+                last = sorted;
+            }
+            head = new_head;
+        }
 };
 
 void bubbleSort(int arr[], int len) {
@@ -84,10 +129,12 @@ void bubbleSort(int arr[], int len) {
 int main() {
     LinkedList list;
     list.append(1);
-    list.append(420);
+    list.append(-420);
     list.append(8086);
     list.printList();
     list.printAddress();
     list.reverseList();
+    list.printList();
+    list.sortList();
     list.printList();
 }
